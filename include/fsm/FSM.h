522e6d64ba422672a7c3af985125129d3dd4c911
ukjae001 <ukjae001@gmail.com>
2026-02-05 00:04:09 +0900

    FSM.h

--- include/fsm/FSM.h
+++ include/fsm/FSM.h
@@ -6,7 +6,6 @@
 #include <memory>
 #include <iostream>
 
-// Unitree SDK2 DDS 관련 헤더
 #include <unitree/robot/channel/channel_publisher.hpp>
 #include <unitree/robot/channel/channel_subscriber.hpp>
 #include <unitree/idl/hg/LowCmd_.hpp>
@@ -15,10 +14,8 @@
 #include "common/Types.h"
 #include "fsm/BaseState.h"
 
-// 네임스페이스 별칭 설정
 using namespace unitree_hg::msg::dds_;
 
-// 🔥 헬퍼 함수 선언 (FSM.cpp에서 정의한 함수를 헤더에서도 알 수 있게 함)
 StateID stringToStateID(std::string id);
 
 class FSM {
@@ -41,7 +38,7 @@ private:
     
     std::unique_ptr<LowCmd_> _lowcmd_msg;
     
-    std::vector<float> _default_kp; // YAML에서 읽어온 기본 게인 저장소
+    std::vector<float> _default_kp; 
     std::vector<float> _default_kd;
 
     void InitLowCmd();
